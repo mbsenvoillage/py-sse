54dde4e5e30e42612071d30cfbf77411763a6277
YvvY <momboisse.yvn@gmail.com>
2022-10-30 10:12:15 +0100

    send data every second

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
+import random
+import time
 
 print(__name__)
 app = Flask(__name__)
@@ -6,3 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/weights")
+def stream():
+    def eventStream():
+        while True:
+            time.sleep(1)
+            print("about to yield")
+            yield "data: hello \n\n"
+    return Response(eventStream(), mimetype="text/event-stream")
